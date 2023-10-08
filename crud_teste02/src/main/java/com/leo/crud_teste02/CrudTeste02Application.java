279c0ce6dacbb1700adf30503f0b71db59be57bf
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2023-10-08 09:52:30 -0300

    Iniciando projeto e conectando com MYSQL

---
+++ crud_teste02/src/main/java/com/leo/crud_teste02/CrudTeste02Application.java
@@ -0,0 +1,13 @@
+package com.leo.crud_teste02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTeste02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudTeste02Application.class, args);
+	}
+
+}
---
+++ crud_teste02/src/test/java/com/leo/crud_teste02/CrudTeste02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.crud_teste02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTeste02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
